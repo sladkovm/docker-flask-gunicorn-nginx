16c29cf071e8443ed0070df68d49a9bdef133b34
sladkovm <sladkovm@gmail.com>
2017-09-07 00:02:50 +0200

    removed static content

--- run_docker.sh
+++ run_docker.sh
@@ -9,4 +9,4 @@ docker-compose build
 echo runnung docker containers
 docker-compose up -d
 
-echo the app is served at 127.0.0.1 on the locahost or at the IP address git sttof the server
+echo the app is served at 127.0.0.1 on the locahost or at the IP address of the server
