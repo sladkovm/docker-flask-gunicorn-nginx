1f70c1bcd2d37b33e4be14d6d6b9464d9853204e
sladkovm <sladkovm@gmail.com>
2017-09-17 22:16:47 +0200

    minor changes

--- flask_api/app.py
+++ flask_api/app.py
@@ -15,5 +15,5 @@ def api_resource():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
 
--- flask_app/app.py
+++ flask_app/app.py
@@ -50,4 +50,4 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8000)
+    app.run_server(debug=True)
