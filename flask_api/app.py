d97a9a2f6688bb24c6c520e33676126e4fe0218b
sladkovm <sladkovm@gmail.com>
2017-09-20 23:26:38 +0200

    added a db

--- flask_api/app.py
+++ flask_api/app.py
@@ -3,22 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
-#
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/test'
-# db = SQLAlchemy(app)
-#
-#
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True)
-#     email = db.Column(db.String(120), unique=True)
-#
-#     def __init__(self, username, email):
-#         self.username = username
-#         self.email = email
-#
-#     def __repr__(self):
-#         return '<User {}>'.format(self.username)
+
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
 @app.route("/")
