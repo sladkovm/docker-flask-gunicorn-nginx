42feec87ae39082e98ed14e6256cf2518d53d3fa
sladkovm <sladkovm@gmail.com>
2018-04-21 23:07:05 +0200

    code layout

--- dash_app/app.py
+++ dash_app/app.py
@@ -17,49 +17,55 @@ colors = {
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
+app.layout = html.Div(
+    style={'backgroundColor': colors['background']},
+    children=[
+        html.H1(
+            children='Hello Dash',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+
+        html.Div(
+            children='Dash: A web application framework for Python.',
+            style={
             'textAlign': 'center',
             'color': colors['text']
-        }
-    ),
+            }
+        ),
 
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'images': [
-                    {
-                        'source':"http://192.168.99.100/static/api_logo_pwrdBy_strava_horiz_light.png",
-                        'xref':"paper",
-                        'yref':"paper",
-                        'x':1,
-                        'y':1.05,
-                        'sizex':0.2,
-                        'sizey':0.2,
-                        'xanchor':"right",
-                        'yanchor':"bottom"
-                }],
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
+        dcc.Graph(
+            id='example-graph-2',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                ],
+                'layout': {
+                    'images': [
+                        {
+                            'source':"http://192.168.99.100/static/api_logo_pwrdBy_strava_horiz_light.png",
+                            'xref':"paper",
+                            'yref':"paper",
+                            'x':1,
+                            'y':1.05,
+                            'sizex':0.2,
+                            'sizey':0.2,
+                            'xanchor':"right",
+                            'yanchor':"bottom"
+                    }],
+                    'plot_bgcolor': colors['background'],
+                    'paper_bgcolor': colors['background'],
+                    'font': {
+                        'color': colors['text']
+                    }
                 }
             }
-        }
-    )
-])
+        )
+    ]
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
