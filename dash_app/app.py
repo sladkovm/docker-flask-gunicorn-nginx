d3d1728946c9772a390c952164619da782a85dd7
sladkovm <sladkovm@gmail.com>
2018-04-21 23:30:29 +0200

    basic routing

--- dash_app/app.py
+++ dash_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 
 server = Flask(__name__)
@@ -17,28 +18,8 @@ colors = {
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(
-    style={'backgroundColor': colors['background']},
-    children=[
-        html.H1(
-            children='Hello Dash',
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }
-        ),
-
-        html.Div(
-            children='Dash: A web application framework for Python.',
-            style={
-            'textAlign': 'center',
-            'color': colors['text']
-            }
-        ),
-
-        dcc.Graph(
-            id='example-graph-2',
-            figure={
+figures = {
+    "fig1": {
                 'data': [
                     {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
                     {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
@@ -63,9 +44,53 @@ app.layout = html.Div(
                     }
                 }
             }
+}
+
+app.layout = html.Div(
+    style={'backgroundColor': colors['background']},
+    children=[
+
+        dcc.Location(id='url', refresh=False),
+
+        html.H1(
+            id='header',
+            children='Hello Dash',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+        ),
+
+        html.Div(
+            children='Dash: A web application framework for Python.',
+            style={
+            'textAlign': 'center',
+            'color': colors['text']
+            }
+        ),
+
+        dcc.Graph(
+            id='fig',
+            figure=figures['fig1']
         )
     ]
 )
 
+@app.callback(Output('header', 'children'), [Input('url', 'pathname')])
+def routing(pathname):
+    """Very basic router
+
+    This callback function will read the current url
+    and pass it's value to the html element with id='header'
+    and set it to the propery 'children'
+
+    Returns:
+        pathname - will pass it to the html element with id='header'
+    """
+
+    return pathname
+
+
 if __name__ == '__main__':
+
     app.run_server(debug=True)
